032c8d95698e20d5e5075e6600ca312fbb6aed12
Josh Mangiola <joshmangiola@Joshs-iMac.gateway>
2020-09-13 10:11:04 +0930

    Initial Commit

---
+++ siteConfig.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Melbourne After Midnight',
+  shortName: 'MAM',
+  description: 'A bold, minimal theme for Gatsby, focused on clean typography & beautiful imagery',
+  url: 'https://gatsby-lam.vaporwavy.io', // Domain of your site without prefix!
+  prefix: '/',
+  author: 'Josh Mangiola',
+  twitter: ''
+}
